9bc50e7cd21091b8c7d289e195e7375f16b7e98b
Mark Hopcraft <mark.hopcraft@madgex.com>
2020-08-14 19:25:57 +0100

    final squash of destiny

---
+++ hello.js
@@ -0,0 +1,29 @@
+// function one here
+// a new line of text under func 1aa
+function one() {
+  console.log('one !')
+}
+
+
+// function two here
+// a new line of text under func 2aa
+function two() {
+  console.log('two !')
+}
+
+
+// function three here
+// a new line of text under func 3aa
+function three() {
+  console.log('three !')
+}
+
+
+// function four here
+// a new line of text under func 4aa
+function four() {
+  console.log('four !')
+}
+
+
+// a comment
