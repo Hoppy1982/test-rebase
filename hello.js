c5aa618aa46cbf063ee654c63583c7b1cd0d43aa
Mark Hopcraft <mark.hopcraft@madgex.com>
2020-08-14 19:25:57 +0100

    final final v2 squash of destiny
    squashed commit msgs
    - add super function five
    - why - reasons
    - side effects - dunno
    - add comment about change to master since branch1 created
    - fix merge conflicts and merge

---
+++ hello.js
@@ -0,0 +1,39 @@
+// function one here
+// a new line of text under func 1aa
+function one() {
+  console.log('one !')
+}
+
+
+// function two here
+// a new line of text under func 2aa
+function two() {
+  console.log('two !')
+}
+
+
+// function three here
+// a new line of text under func 3aa
+function three() {
+  console.log('three !')
+}
+
+
+// function four here
+// a new line of text under func 4aa
+function four() {
+  console.log('four !')
+}
+
+
+// function five here
+// a new line of text under func 5aa
+function five() {
+  console.log('five !')
+}
+
+
+/* something wot has changed in master branch since branch1 was 
+created */
+
+// Branch1 feature will go here!
